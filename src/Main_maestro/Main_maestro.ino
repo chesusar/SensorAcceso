f03a98fbe232e59358c06cc3dcf9807efc35f121
Raúl Arribas Antón <raul.arribas4@gmail.com>
2024-03-22 10:17:24 +0100

    comit 2

--- src/Main_maestro/Main_maestro.ino
+++ src/Main_maestro/Main_maestro.ino
@@ -1,11 +1,46 @@
-int LED = 13;
-void setup() {
-  pinMode(LED, OUTPUT);
+#include <Arduino.h>
+
+////Librerias dfplayer////
+#include "Alarma.h"
+
+////Librerias RFID////
+#include "UID.h"
+
+////Instancias dfplayer////
+int activa = 0;
+
+////Instancias RFID////
+String UID = "";
+String UID_valido = " f3 5a 4c 17";
+
+////Instancias comunicacion////
+int recibido = 0;
+
+void setup(){
+  Serial.begin(115200);
+
+////Configurar dfplayer////
+  Config_alarma();
 }
 
-void loop() {
-  digitalWrite(LED, HIGH);
-  delay(100);
-  digitalWrite(LED, LOW);
-  delay(100);
+void loop(){
+  if(recibido == 1){
+    if(activa == 0){
+      Activar_alarma();
+      activa = 1;
+    }
+    else if(activa == 1){
+      Bucle_alarma();
+    }
+
+    UID = leer_UID();
+
+    if(UID == UID_valido){        // Añadir "o tecleado contraseña correcta"
+      Serial.println("Valido");
+      activa = 0;
+      recibido = 0;
+      Desactivar_alarma();
+    }
+
+  }
 }
--- src/RFID/RFID.ino
+++ src/RFID/RFID.ino
@@ -1,7 +1,11 @@
-#include <SPI.h>
-#include <MFRC522.h>
+#include <Arduino.h>
+
+////Librerias RFID////
 #include "UID.h"
 
+////Instancias RFID////
+String UID = "";
+String UID_valido = " f3 5a 4c 17";
 
 void setup() {
   Serial.begin(115200);
@@ -9,6 +13,11 @@ void setup() {
 }
 
 void loop() {
-  leer_UID();
+  UID = leer_UID();
+  Serial.println(UID);
+  
+  if(UID == UID_valido){
+    Serial.println("Valido");
+  }
   delay(500);
 }
